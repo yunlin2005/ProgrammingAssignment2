ddff9e5aeed4130b91b34bf31dec6088b9d2dd96
yunlin2005 <yun.lin2005@gmail.com>
2014-10-24 08:56:59 +0200

    Yun Lin's commit for the R programming assignment
    https://class.coursera.org/rprog-008/human_grading/view/courses/972581/assessments/3/submissions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
+cachedInverse <- NULL
+cachedMatrix <- NULL
+## makeCacheMatix defines a list of functions to set and get matix, 
 makeCacheMatrix <- function(x = matrix()) {
-
+     set <- function(y) {
+       x <<- y
+       cachedMatrix <<- x
+       cachedInverse <<- NULL
+     }    
+     get <- function() x 
+     getcachedMatrix <- function() cachedMatrix
+     setinverse <- function(inverse) cachedInverse <<- inverse
+     getinverse <- function() cachedInverse
+     list(set = set, get = get, getcachedMatrix = getcachedMatrix, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve function to returns the inverse matrix of input matrix. If they are already in the cache, 
+## the function returns the cache , otherwise computes the inverse matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    input_matrix <- x$get()
+    inverse_matrix <- x$getinverse()
+    cached_matrix <- x$getcachedMatrix()
+    
+    if(!is.null(inverse_matrix) && isTRUE(all.equal(cached_matrix,input_matrix))){
+        message("getting the cached inverse matrix")
+        return(inverse_matrix)
+    }
+    
+    x$set(input_matrix)
+    inverse_matrix <- solve(input_matrix, ...)
+    x$setinverse(inverse_matrix)
+    inverse_matrix
 }
