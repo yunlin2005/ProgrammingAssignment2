bb460d55ca6248304689e0b35b5bbf4e7e3fed1a
yunlin2005 <yun.lin2005@gmail.com>
2014-10-24 09:31:02 +0200

    Yun Lin's commit of R Programming Assignment 2
    https://class.coursera.org/rprog-008/human_grading/view/courses/972581/assessments/3/submissions

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function defines a list of functions to set and get matix,
+## cacheSolve function to return the inverse matrix of input matrix. If they are already in the cache, 
+## the function returns the cache , otherwise computes the inverse matrix
+
+## Initial the cache variables
 cachedInverse <- NULL
 cachedMatrix <- NULL
-## makeCacheMatix defines a list of functions to set and get matix, 
+
+## makeCacheMatix defines a list of functions:
+## set: to set the input matrix, and cache it to the cache variables, reset the inverse matrix to NULL
+## get: to get the input matrix
+## getcachedMatrix: to get the cached Matrix from the previous input
+## setinverse: set the inverse matrix to the cached variable
+## getinverse: get the cached inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
      set <- function(y) {
        x <<- y
@@ -17,10 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve function to returns the inverse matrix of input matrix. If they are already in the cache, 
-## the function returns the cache , otherwise computes the inverse matrix
+##cacheSolve function take the makeCacheMatix as the input, comparing the input matrix with the cached matrix. 
+## If the matrix  are already in the cache, return the cached inverse matrix
+## otherwise set the new input matrix, compute the inverse matrix, set the computed inverse matrix to the cache variable
+## and return the computed inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
     input_matrix <- x$get()
     inverse_matrix <- x$getinverse()
     cached_matrix <- x$getcachedMatrix()
